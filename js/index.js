7daced72d23be1c9773aae60dca868f5ca075159
VedovatoHenrique <henriquevedovato97@gmail.com>
2022-07-11 19:11:31 -0300

    Finalização Projeto

--- js/index.js
+++ js/index.js
@@ -15,6 +15,71 @@ const audioRain = new Audio("./sounds/Chuva.wav")
 const audioFire = new Audio("./sounds/Lareira.wav")
 const audioCoffeHouse = new Audio("./sounds/Cafeteria.wav")
 const kitchenTimer = new Audio("./sounds/audios_kichen-timer.mp3")
+const wallpaper =  document.getElementById('wallpaper')
+const mode = document.querySelector('.modulos')
+const clear = document.querySelector('body')
+const dia = document.querySelector('.dia')
+const noite = document.querySelector('.dark')
+const hide = document.querySelector('.hide')
+
+function modelight(){
+  clear.classList.add('light')
+  dia.classList.add('hide')
+  noite.classList.remove('hide')
+}
+function modeDark(){
+  clear.classList.remove('light')
+  dia.classList.remove('hide')
+  noite.classList.add('hide')
+}
+
+
+
+function updateWallpaperForrest(){
+  if(wallpaper.classList.contains('img-florest')){
+    wallpaper.classList.remove('img-florest')
+    wallpaper.classList.add('fundo')
+  }else{
+  wallpaper.classList.add('img-florest')
+  wallpaper.classList.remove('img-coffe')
+  wallpaper.classList.remove('img-fire')
+  wallpaper.classList.remove('img-rain')
+  wallpaper.classList.remove('fundo')
+}}
+function updateWallpaperFire(){
+  if(wallpaper.classList.contains('img-fire')){
+    wallpaper.classList.remove('img-fire')
+    wallpaper.classList.add('fundo')
+  }else{
+  wallpaper.classList.remove('img-florest')
+  wallpaper.classList.remove('img-coffe')
+  wallpaper.classList.add('img-fire')
+  wallpaper.classList.remove('img-rain')
+  wallpaper.classList.remove('fundo')
+}}
+function updateWallpaperCoffe(){
+  if(wallpaper.classList.contains('img-coffe')){
+    wallpaper.classList.remove('img-coffe')
+    wallpaper.classList.add('fundo')
+  }else{
+  wallpaper.classList.remove('img-florest')
+  wallpaper.classList.add('img-coffe')
+  wallpaper.classList.remove('img-fire')
+  wallpaper.classList.remove('img-rain')
+  wallpaper.classList.remove('fundo')
+}}
+function updateWallpaperrain(){
+  if(wallpaper.classList.contains('img-rain')){
+    wallpaper.classList.remove('img-rain')
+    wallpaper.classList.add('fundo')
+    
+  }else{
+  wallpaper.classList.remove('img-florest')
+  wallpaper.classList.remove('img-coffe')
+  wallpaper.classList.remove('img-fire')
+  wallpaper.classList.add('img-rain')
+  wallpaper.classList.remove('fundo')
+}}
 
 function countdown() {
   timerTimeOut = setTimeout(function () {
@@ -137,16 +202,76 @@ function focusFire(){
       fire.classList.add('focus')
     }
 }
+
+function volume(){
+  update = setTimeout(function(){
+    switch (forrest.classList.contains('focus')){
+      case true:
+        percentageForrest = document.querySelector('.volumeForest')
+        audioForrest.volume = percentageForrest.value / 100
+        break
+      default:
+        document.querySelector('.volumeForest').value = 50
+        audioForrest.volume = 0.5
+        break
+    }
+    switch (rain.classList.contains('focus')){
+      case true:
+      percentageRain = document.querySelector('.volumeRain')
+      audioRain.volume = percentageRain.value / 100
+      break
+      default:
+        document.querySelector('.volumeRain').value = 50
+        audioRain.volume = 0.5
+        break
+    }
+    switch (coffeHouse.classList.contains('focus')){
+      case true:
+      percentageCoffe = document.querySelector('.volumeCoffe')
+      audioCoffeHouse.volume = percentageCoffe.value / 100
+      break
+      default:
+        document.querySelector('.volumeCoffe').value = 50
+        audioCoffeHouse.volume = 0.5
+        break
+    }
+    switch (fire.classList.contains('focus')){
+      case true:
+      percentageFire = document.querySelector('.volumeFire')
+      audioFire.volume = percentageFire.value / 100
+      break
+      default:
+        document.querySelector('.volumeFire').value = 50
+        audioFire.volume = 0.5
+        break
+    }
+    volume()
+  }, 1)
+}
+
+volume()
+
+
 forrest.addEventListener('click', function(){
-  focusForrest()
+  focusForrest() 
+  updateWallpaperForrest() 
 })
 rain.addEventListener('click', function(){
-  focusRain()
+  focusRain() 
+  updateWallpaperrain()
+  
 })
 coffeHouse.addEventListener('click', function(){
   focusCoffeHouse()
+  updateWallpaperCoffe()
 })
 fire.addEventListener('click', function(){
   focusFire()
+  updateWallpaperFire()
 })
-
+dia.addEventListener('click', function(){
+  modelight()
+})
+noite.addEventListener('click', function(){
+  modeDark()
+})
